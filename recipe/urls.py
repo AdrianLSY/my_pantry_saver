8a53db8980657796b184f7df3600d52e39fcef80
Peter <59990318+twausping@users.noreply.github.com>
2021-08-27 17:49:56 +1000

    Completed Recipe Model
    List, View, Create, Edit, Delete

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import RecipeList
+from .views import RecipeList, RecipeDetail, RecipeCreate, RecipeUpdate, RecipeDelete
 
 urlpatterns = [
-    path('', RecipeList.as_view(), name='recipe'), 
+    path('', RecipeList.as_view(), name='recipelist'), 
+    path('recipe/<int:pk>', RecipeDetail.as_view(), name='recipe'), 
+    path('new-recipe/', RecipeCreate.as_view(), name='new-recipe'), 
+    path('edit-recipe/<int:pk>', RecipeUpdate.as_view(), name='edit-recipe'), 
+    path('delete-recipe/<int:pk>', RecipeDelete.as_view(), name='delete-recipe'), 
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Recipe
 
 class RecipeList(ListView):
     model = Recipe
     context_object_name = 'recipes'
+
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipe/recipe.html'
+
+class RecipeCreate(CreateView):
+    model = Recipe
+    fields = '__all__'
+    success_url = reverse_lazy('recipelist')
+
+class RecipeUpdate(UpdateView):
+    model = Recipe
+    fields = '__all__'
+    success_url = reverse_lazy('recipelist')
+
+class RecipeDelete(DeleteView):
+    model = Recipe
+    context_object_name = 'recipe'
+    success_url = reverse_lazy('recipelist')
