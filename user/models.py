c94e4b4590bf15dac993be0537b2f32bdc8d14b6
ethan-lin-l <ethanlin821107@gmail.com>
2021-08-29 15:58:25 +0800

    user-model

---
+++ user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class User(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    id= models.IntegerField(primary_key=True)
+    recipe=models.ForeignKey('recipe')
+    ingredient=models.ForeignKey('ingredient')
+
+
+    def __str__(self):
+        return self.username 
+
+    class Meta:
+        ordering=['id']
+        
