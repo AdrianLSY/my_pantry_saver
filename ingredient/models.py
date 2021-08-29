b79d7133741a9f99ed6046faacc9471818b132a7
YiChienYu <yichienyu1002@gmail.com>
2021-08-29 15:54:29 +0800

    model add recipe foreign key

--- ingredient/models.py
+++ ingredient/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-    primary = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100) 
+    primary_id = models.IntegerField(primary_key=True, blank=False)
+    name = models.CharField(max_length=100)
+    # recipe = models.ForeignKey('recipe')
     type = models.IntegerField(blank=False)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        order = ['primary'] # Also can be sorted by name
+        ordering = ['name'] # Also can be sorted by primary_id
