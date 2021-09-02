c2f56b55fc37a9e32052af87f56f3aefbb96f049
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-02 20:41:45 +0800

    quick bug fix

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RecipeList, RecipeDetail, RecipeCreate, RecipeUpdate, RecipeDelete
 
 urlpatterns = [
-    path('recepe/', RecipeList.as_view(), name='recipelist'), 
+    path('recipe/', RecipeList.as_view(), name='recipelist'), 
     path('recipe/<int:pk>', RecipeDetail.as_view(), name='recipe'), 
     path('new-recipe/', RecipeCreate.as_view(), name='new-recipe'), 
     path('edit-recipe/<int:pk>', RecipeUpdate.as_view(), name='edit-recipe'), 
