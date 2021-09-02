6b20f9fd01605cc6e894c1b7abdb878103352285
ethan-lin-l <ethanlin821107@gmail.com>
2021-09-02 12:31:43 +0800

    view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView 
+from django.views.generic import FormView
 
+
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login 
+from .models  import User
 # Create your views here.
+
+
+class UserLoginView(LoginView):
+    template_name='base/login.html'
+    field ='__all__ '
+    redirect_authenticated_user = True
+    sucess_url =
+    
+
+
+class UserRegister(FormView):
+    template_name = 'base/register'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    sucess_url =
+    
