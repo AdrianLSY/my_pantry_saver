6c8e3ca82bf26b890cad12143e7febb1a6d5e900
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-09 00:55:47 +0800

    added some tweaks
    fix bug causing urls to head to 404
    added null=True, blank=True to models.py

--- ingredient/models.py
+++ ingredient/models.py
@@ -5,7 +5,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     # recipe = models.ForeignKey('recipe')
     type = models.IntegerField(blank=False)
-    datetime = models.DateField(blank=False)
+    expirey_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
