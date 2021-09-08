c8b549dd27f0d813fd6931fce60e5f75943efb5a
YiChienYu <yichienyu1002@gmail.com>
2021-09-08 20:03:31 +0800

    Add User_Ingredient and Recipe_Ingredient model

--- ingredient/models.py
+++ ingredient/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from recipe.models import Recipe
+from user.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-    # recipe = models.ForeignKey('recipe')
     type = models.IntegerField(blank=False)
     datetime = models.DateField(blank=False)
 
@@ -12,3 +13,17 @@ class Ingredient(models.Model):
 
     class Meta:
         ordering = ['name'] # Also can be sorted by primary_id
+
+class User_Ingredient(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    ingredient= models.ForeignKey(Ingredient, on_delete = models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+class Recipe_Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+    ingredient= models.ForeignKey(Ingredient, on_delete = models.SET_NULL)
+
+    def __str__(self):
+        return self.name
