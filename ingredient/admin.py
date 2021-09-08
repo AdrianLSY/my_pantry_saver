0a126c2e960cbb7ecc1fac321c1bf2ef1f859b9c
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-09 00:03:22 +0800

    Revert "Merge branch 'ingredient-model' of https://github.com/AdrianLSY/my_pantry_saver into ingredient-model"
    This reverts commit 6b7b056bddc5711d734f7ce55196d861ced8a459, reversing
    changes made to 3c5f56ff6ffbd22ca0598afb075880325603f2e9.

--- ingredient/admin.py
+++ ingredient/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Ingredient, User_Ingredient, Recipe_Ingredient
+from .models import Ingredient
 
-admin.site.register(Ingredient)
-admin.site.register(User_Ingredient)
-admin.site.register(Recipe_Ingredient)
+admin.register(Ingredient)
