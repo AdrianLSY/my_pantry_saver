0aa54e27f267f3d1de7e4218740ca3fc6eec2d6a
Peter <59990318+twausping@users.noreply.github.com>
2021-09-15 00:54:40 +1000

    User model (#6)
    * Create test.txt
    
    * first_try
    
    * user-model
    
    * changed __str__
    
    * admin register
    
    * view
    
    * https://github.com/AdrianLSY/my_pantry_saver
    
    * login view
    
    * register
    
    * view
    
    * view2
    
    * add user restrict
    
    * Completed user Model
    
    Some lines of code were commented out and they can be used in the future to get client specific data.
    
    Co-authored-by: AdrianLSY <sites.ad.p3@gmail.com>
    Co-authored-by: ethan-lin-l <ethanlin821107@gmail.com>

--- my_pantry_saver/urls.py
+++ my_pantry_saver/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('user.urls')), 
     path('', include('ingredient.urls')),
     path('', include('recipe.urls')),
     path('', include('landing.urls'))
