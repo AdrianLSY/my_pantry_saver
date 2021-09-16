aadb106eea96109c8b815363bfa69c6117c18fc9
YiChienYu <52337683+YiChienYu@users.noreply.github.com>
2021-09-16 15:02:03 +0800

    add relationship model(views have to be complete)

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from recipe.models import Recipe
 # Create your models here.
 
 
@@ -17,5 +18,15 @@ class User(models.Model):
     #    ordering=['id']
 
 
+class UserRecipe(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('recipe', 'user'), name='Recipe_per_user')
+        ]
+
+    def get(self):
+        return self.recipe
 
