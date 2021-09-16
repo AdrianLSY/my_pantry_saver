35e4eaa24c995a8741f1061256b90d9b9dec5337
Peter <59990318+twausping@users.noreply.github.com>
2021-09-16 20:53:17 +1000

    Add User Ingredient

--- user/models.py
+++ user/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
+from ingredient.models import Ingredient
+from recipe.models import Recipe
 # Create your models here.
 
 
-class User(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    #recipe = models.ForeignKey('recipe', on_delete=models.CASCADE)
-    #ingredients = models.ForeignKey('ingredient', on_delete=models.CASCADE)
+# class User(models.Model):
+#     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     
+#     def __str__(self):
+#         return self.user
 
-    def __str__(self):
-        return self.user
+class UserIngredient(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    #recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    expirey_date = models.DateField(null=True, blank=True)
+    quantity = models.IntegerField(null=True)
 
-    #class Meta:
-    #    ordering=['id']
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('user', 'ingredients'), name='user_ingredients')
+        ]
+
+    def get(self):
+        return self.ingredients
 
 
 
--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
+from ingredient.models import Ingredient
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login 
-from .models import User
+from .models import UserIngredient
 from django.views.generic.list import ListView 
 
 
@@ -43,16 +44,31 @@ class UserRegister(FormView):
     
 
 
-class MyPantry(ListView):
-#class MyPantry(LoginRequiredMixin, ListView): if we want login required for the main page.
-    model = User 
+
+class MyPantry(LoginRequiredMixin, ListView):
+    model = UserIngredient
     template_name = 'user/mypantry.html'
+    context_object_name = 'ingredient'
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        #filtering data to get user specific data
-        #context ['recipe'] =context['recipe'].filter(user=self.request.user)
-        #context ['ingredient'] =context['ingredient'].filter(user=self.request.user)
+        context = super().get_context_data(**kwargs)
+        #context['recipe'] = context['recipe'].filter(user=self.request.user)
+        context['ingredient'] = context['ingredient'].filter(user=self.request.user)
         return context 
-    
-    
+
+class UserIngredientCreate(CreateView):
+    model = UserIngredient
+    fields = '__all__'
+    template_name = 'user_ingredients/ingredient_form.html'
+    success_url = reverse_lazy('mypantry')
+    context_object_name = 'ingredient_list'
+
+class UserIngredientUpdate(UpdateView):
+    model = UserIngredient
+    fields = '__all__'
+    success_url = reverse_lazy('mypantry')
+
+class UserIngredientDelete(DeleteView):
+    model = UserIngredient
+    context_object_name = 'ingredient'
+    success_url = reverse_lazy('mypantry')
