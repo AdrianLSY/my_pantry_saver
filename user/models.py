b53bcd51dc1e0c0aba9dd557729fc97550378b56
YiChienYu <52337683+YiChienYu@users.noreply.github.com>
2021-09-20 19:17:43 +0800

    modify views of MyPantry to allow use to access both ingredient and recipe

--- user/models.py
+++ user/models.py
@@ -2,31 +2,32 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 from recipe.models import Recipe
+from ingredient.models import Ingredient
 # Create your models here.
 
 
-class User(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    #recipe = models.ForeignKey('recipe', on_delete=models.CASCADE)
+#class User(models.Model):
+    #user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    #recipe = models.ManyToManyField(Recipe, through="UserRecipe")
     #ingredients = models.ForeignKey('ingredient', on_delete=models.CASCADE)
     
 
-    def __str__(self):
-        return self.user
+    #def __str__(self):
+     #   return self.user
 
     #class Meta:
-    #    ordering=['id']
+      # ordering=['id']
 
 
 class UserRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=('recipe', 'user'), name='Recipe_per_user')
-        ]
-
+        #constraints = [
+        #    models.UniqueConstraint(fields=('recipe', 'user'), name='Recipe_per_user')
+        #]
+        ordering = ['recipe']
     def get(self):
         return self.recipe
 
--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import UserLoginView, UserRegister, MyPantry
-
+from .views import UserLoginView, UserRegister, MyPantry, UserRecipeCreate, UserRecipeUpdate, UserRecipeDelete
 from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
@@ -8,5 +7,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='mypantry'), name='logout'),
     #path('logout/', LogoutView.as_view(next_page='login'), name='logout'), if we want to redirect to login page.
     path('register/', UserRegister.as_view(), name='register'),
-    path('mypantry', MyPantry.as_view(), name= 'mypantry')  
+    path('mypantry', MyPantry.as_view(), name= 'mypantry'),
+    path('userrecipe/new', UserRecipeCreate.as_view(), name='user-add-recipe'), 
+    path('user_recipe/edit/<int:pk>', UserRecipeUpdate.as_view(), name='user-edit-recipe'), 
+    path('user_recipe/delete/<int:pk>', UserRecipeDelete.as_view(), name='user-delete-recipe'), 
 ]
