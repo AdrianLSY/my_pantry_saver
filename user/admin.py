3360fd3b5daeb26fb23da1eebe9a210767c861db
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-21 18:04:33 +0800

    Squashed commit of the following:
    commit eb931eec4b3194d644401b36a0952036cff04412
    Author: YiChienYu <52337683+YiChienYu@users.noreply.github.com>
    Date:   Tue Sep 21 09:12:25 2021 +0800
    
        Display recipe and ingredient on mypantry
    
    commit b53bcd51dc1e0c0aba9dd557729fc97550378b56
    Author: YiChienYu <52337683+YiChienYu@users.noreply.github.com>
    Date:   Mon Sep 20 19:17:43 2021 +0800
    
        modify views of MyPantry to allow use to access both ingredient and recipe
    
    commit aadb106eea96109c8b815363bfa69c6117c18fc9
    Author: YiChienYu <52337683+YiChienYu@users.noreply.github.com>
    Date:   Thu Sep 16 15:02:03 2021 +0800
    
        add relationship model(views have to be complete)

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserIngredient 
+from .models import UserRecipe, UserIngredient
 
+#admin.site.register(User)
+admin.site.register(UserRecipe)
 admin.site.register(UserIngredient)
--- user/models.py
+++ user/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 from django import forms
 from django.contrib.auth.models import User
-from ingredient.models import Ingredient
 from recipe.models import Recipe
+from ingredient.models import Ingredient
 # Create your models here.
 
 
-# class User(models.Model):
-#     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+#class User(models.Model):
+    #user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    #recipe = models.ManyToManyField(Recipe, through="UserRecipe")
+    #ingredients = models.ForeignKey('ingredient', on_delete=models.CASCADE)
     
-#     def __str__(self):
-#         return self.user
+
+    #def __str__(self):
+     #   return self.user
+
+    #class Meta:
+      # ordering=['id']
+
+
+class UserRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+
+    class Meta:
+        #constraints = [
+        #    models.UniqueConstraint(fields=('recipe', 'user'), name='Recipe_per_user')
+        #]
+        ordering = ['recipe']
+    def get(self):
+        return self.recipe
 
 class UserIngredient(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -27,7 +46,3 @@ class UserIngredient(models.Model):
 
     def get(self):
         return self.ingredients
-
-
-
-
