55b7c06fd54c53f9735572e8c90128acd63dd8e9
Peter <59990318+twausping@users.noreply.github.com>
2021-09-21 19:58:45 +1000

    User ingredient relationship (#9)
    * Add User Ingredient
    
    * Completed User Ingredient
    
    Co-authored-by: AdrianLSY <sites.ad.p3@gmail.com>

--- my_pantry_saver/settings.py
+++ my_pantry_saver/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'my_pantry_saver.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_pantry_saver',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
