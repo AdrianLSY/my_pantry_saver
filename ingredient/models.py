761193abbcb4dd255b7ca0dcb00a7fa43dbb781d
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-21 17:56:15 +0800

    removed Ingredient.type

--- ingredient/models.py
+++ ingredient/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     # recipe = models.ForeignKey('recipe')
-    type = models.IntegerField(blank=False)
     expirey_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
--- recipe/urls.py
+++ recipe/urls.py
@@ -4,7 +4,7 @@ from .views import RecipeList, RecipeDetail, RecipeCreate, RecipeUpdate, RecipeD
 urlpatterns = [
     path('user/<int:pk>', UserDetail.as_view(), name='user'), 
     path('users/', UserList.as_view(), name='userlist'), 
-    path('recepes/', RecipeList.as_view(), name='recipelist'), 
+    path('recipes/', RecipeList.as_view(), name='recipelist'), 
     path('recipe/<int:pk>', RecipeDetail.as_view(), name='recipe'), 
     path('recipe/new', RecipeCreate.as_view(), name='new-recipe'), 
     path('recipe/edit/<int:pk>', RecipeUpdate.as_view(), name='edit-recipe'), 
