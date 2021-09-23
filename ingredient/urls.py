cca35a84e20fbea9377bc9410bd346cb350a9f76
AdrianLSY <sites.ad.p3@gmail.com>
2021-09-23 15:38:08 +0800

    updated urls

--- ingredient/urls.py
+++ ingredient/urls.py
@@ -4,7 +4,7 @@ from .views import IngredientDetail, IngredientList, IngredientCreate, Ingredien
 urlpatterns = [
     path('ingredients/', IngredientList.as_view(), name='ingredients'),
     path('ingredient/<int:pk>/', IngredientDetail.as_view(), name='ingredient'),
-    path('ingredient/new', IngredientCreate.as_view(), name='new-ingredient'), 
-    path('ingredient/edit/<int:pk>', IngredientUpdate.as_view(), name='edit-ingredient'), 
-    path('ingredient/delete/<int:pk>', IngredientDelete.as_view(), name='delete-ingredient'), 
+    path('ingredient/new/', IngredientCreate.as_view(), name='new-ingredient'), 
+    path('ingredient/edit/<int:pk>/', IngredientUpdate.as_view(), name='edit-ingredient'), 
+    path('ingredient/delete/<int:pk>/', IngredientDelete.as_view(), name='delete-ingredient'), 
 ]
